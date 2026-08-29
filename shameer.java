d65ae0bcaa34b1318db180474ecc25ac05fc6ba0
shaik shameer vali <shaikshameervali@shaiks-MacBook-Air.local>
2026-08-29 06:41:01 +0530

    shameer

--- shameer.java
+++ shameer.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
-public class practice3{
+public class shameer{
     public static void main(String[] args){
-        String name;
         Scanner r=new Scanner(System.in);
-        System.out.println("enter a string:");
-        name=r.next();
-        StringBuilder sb=new StringBuilder(name);
-        sb.reverse().toString();
-        System.out.println(sb);
+        int a ,b,n,i,temp;
+        System.out.println("Enter n value to repeat");
+        n=r.nextInt();
+        System.out.println("Enter a,b values");
+        a=r.nextInt();
+        b=r.nextInt();
+        for( i=0;i<n;i++){
+           System.out.println(a+" ");
+           temp =a+b;
+           a=b;
+           b=temp;
+        } 
+        System.out.println(b);
         r.close();
     }
 }
